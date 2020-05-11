cab932fe2ee1e83cbf6523012eab6b8905556bdc
Stole Mlekokrad <bodan1921@hotmail.com>
2020-05-12 00:28:40 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, redirect
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def red():
+    return redirect('/index')
+
+@app.route('/index', methods = ['GET', 'POST'])
+def index():
+    response = ""
+    if request.method == 'POST':
+        ingr = request.form['sostojki']
+        if not ingr:
+            return render_template('index.html', sostojki = "")
+        url = 'http://www.recipepuppy.com/api/?i={sostojki}'.format(sostojki = ingr)
+        try:
+            response = requests.get(url)
+            response = response.json()
+        except Exception:
+            return render_template('index.html', notfound = "Couldn't find any recepie")
+    return render_template('index.html', sostojki = response['results'])
+
+if __name__ == "__name__":
+    app.run(debug = True)
