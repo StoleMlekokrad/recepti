7132e04e2fbe6da828c1fbcaacfb5924374617d4
Stole Mlekokrad <bodan1921@hotmail.com>
2020-05-13 00:21:48 +0200

    ready yes?

--- main.py
+++ main.py
@@ -11,16 +11,17 @@ def red():
 def index():
     if request.method == 'POST':
         ingr = request.form['sostojki']
+        #print(ingr)
         if not ingr:
-            return render_template('index.html', ingredients = "")
+            return render_template('index.html', ingredients = "", notfound = "EMPTY")
         url = 'http://www.recipepuppy.com/api/?i={sostojki}'.format(sostojki = ingr)
         try:
             response = requests.get(url)
             response = response.json()
         except Exception:
             return render_template('index.html', notfound = "Couldn't find any recepie")
-        return render_template('index.html', sostojki = response['results'])
-    return render_template('index.html', sostojki = "")
+        return render_template('index.html', sostojki = response["results"])
+    return render_template('index.html', sostojki = "", notfound = "HELLO1")
 
 if __name__ == "__name__":
     app.run()
