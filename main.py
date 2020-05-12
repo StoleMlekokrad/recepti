ad7bdee5b78c8b89535d63e1884ed6b22c6decda
nora <nora.nikoloska9@gmail.com>
2020-05-12 20:01:01 +0200

    Rename ingredients template variable

--- main.py
+++ main.py
@@ -14,14 +14,14 @@ def index():
     if request.method == 'POST':
         ingr = request.form['sostojki']
         if not ingr:
-            return render_template('index.html', sostojki = "")
+            return render_template('index.html', ingredients = "")
         url = 'http://www.recipepuppy.com/api/?i={sostojki}'.format(sostojki = ingr)
         try:
             response = requests.get(url)
             response = response.json()
         except Exception:
             return render_template('index.html', notfound = "Couldn't find any recepie")
-    return render_template('index.html', sostojki = response['results'])
+    return render_template('index.html', ingredients = response['results'])
 
 if __name__ == "__name__":
     app.run()
